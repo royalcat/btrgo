582c0e09e5107609ea1614e6814dd418ba13bdd1
royalcat <k.adamovich20@gmail.com>
2022-10-26 17:50:12 +0300

    values of map

--- map.go
+++ map.go
@@ -12,6 +12,18 @@ func KeysOfMap[K comparable, V any](m map[K]V) []K {
 	return keys
 }
 
+func ValuesOfMap[K comparable, V any](m map[K]V) []V {
+	values := make([]V, len(m))
+
+	i := 0
+	for _, v := range m {
+		values[i] = v
+		i++
+	}
+
+	return values
+}
+
 func FilterMap[K comparable, V any](keys []K, data map[K]V) map[K]V {
 	newMap := make(map[K]V, len(keys))
 
